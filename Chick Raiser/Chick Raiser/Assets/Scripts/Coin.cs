f0f51dd3e0387d999027fe88e3aa4826173b3c4a
Irtaza <irtazanaqvi05@gmail.com>
2024-08-01 10:34:03 +0500

    added score and buying

--- Chick Raiser/Chick Raiser/Assets/Scripts/Coin.cs
+++ Chick Raiser/Chick Raiser/Assets/Scripts/Coin.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+
+    public GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
     void OnMouseDown()
     {
         // Add code to increment the player's coin count
+         gameManager.AddCoin();
         Destroy(gameObject);
+        
     }
 }
---
+++ Chick Raiser/Chick Raiser/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public int coinCount = 0;
+    public TextMeshProUGUI coinText;
+    public GameObject chickPrefab;
+
+    void Update()
+    {
+        coinText.text = "Coins: " + coinCount;
+    }
+
+    public void AddCoin()
+    {
+        coinCount++;
+    }
+
+    public void BuyEgg()
+    {
+        if (coinCount >= 10)
+        {
+            coinCount -= 10;
+            Instantiate(chickPrefab, GetRandomPosition(), Quaternion.identity);
+        }
+    }
+
+    Vector3 GetRandomPosition()
+    {
+        return new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0);
+    }
+}
