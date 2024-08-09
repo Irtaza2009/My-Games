4b6260a4a21bf6543554179f18e9ac7ca5879f4c
Irtaza <irtazanaqvi05@gmail.com>
2024-08-09 22:01:53 +0500

    added velocity and score and highScore

--- Snake/index.js
+++ Snake/index.js
@@ -1,3 +1,5 @@
+const { update } = require("three/examples/jsm/libs/tween.module.js");
+
 const playBoard = document.querySelector(".play-board");
 const scoreElement = document.querySelector(".score");
 const highScoreElement = document.querySelector(".high-score");
@@ -7,3 +9,71 @@ let gameover = false;
 let foodX, foodY;
 let snakeX = 5,
   snakeY = 5;
+let velocityX = 0,
+  velocityY = 0;
+let snakeBody = [];
+let setIntervalId;
+let score = 0;
+
+// high score from local storage
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreElement.innerText = `High Score: ${highScore}`;
+
+// random food position
+
+const updateFoodPosition = () => {
+  foodX = Math.floor(Math.random() * 30) + 1;
+  foodY = Math.floor(Math.random() * 30) + 1;
+};
+
+const handleGameOver = () => {
+  clearInterval(setIntervalId);
+  alert("Game Over! Press OK to restart the game");
+  location.reload();
+};
+
+// changing velocity based on key press
+
+const changeDirection = (e) => {
+  if (e.key === "ArrowUp" && velocityY !== 1) {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (e.key === "ArrowDown" && velocityY !== -1) {
+    velocityX = 0;
+    velocityY = 1;
+  } else if (e.key === "ArrowLeft" && velocityX !== 1) {
+    velocityX = -1;
+    velocityY = 0;
+  } else if (e.key === "ArrowRight" && velocityX !== -1) {
+    velocityX = 1;
+    velocityY = 0;
+  }
+};
+
+// change direction on each key click
+
+controls.forEach((button) =>
+  button.addEventListener("click", () =>
+    changeDirection({ key: button.dataset.key })
+  )
+);
+
+const initGame = () => {
+  if (gameover) return handleGameOver();
+  let html = `<div class = "food" style ="grid-area: ${foodY}/ ${foodX}"></div>`;
+
+  // when snake eats the food
+
+  if (snakeX === foodX && snakeY === foodY) {
+    updateFoodPosition();
+    snakeBody.push([foodY, foodX]); // add food to snake body array
+    score++;
+    highScore = score >= highScore ? score : highScore;
+    localStorage.setItem("high-score", highScore);
+    scoreElement.innerText = `Score: ${score}`;
+    highScoreElement.innerText = `High Score: ${highScore}`;
+  }
+
+  // update snake body
+  //
+};
